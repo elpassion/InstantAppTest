d1c1854a0d4144fab5e102485abca90e5c01e8af
macPry <maciej.przybyl@elpassion.pl>
2017-11-15 14:40:05 +0100

    Project setup

---
+++ features/home/src/main/java/pl/elpassion/instantapptest/features/home/MainActivity.kt
@@ -0,0 +1,21 @@
+package pl.elpassion.instantapptest.features.home
+
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainButton.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://elpassion.com/detils")).apply {
+                `package` = packageName
+                addCategory(Intent.CATEGORY_BROWSABLE)
+            })
+        }
+    }
+}
